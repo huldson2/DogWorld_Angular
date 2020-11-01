8c95fab64b076033542f321d64d9b745dc25f8c7
huldson2 <huldson2@gmail.com>
2020-11-01 12:23:14 -0300

    Ajustes nas páginas criadas
    routeando as página pelo app.modulo.ts e alguns pequenos ajustes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,59 @@
+// importar as dependencias
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { E404Component } from './pages/e404/e404.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/news/news.component';
 
-const routes: Routes = [];
+// constante com as rotas 
+const routes: Routes = [
+
+// Rota padrão da página inicial
+{
+  path: '',
+  redirectTo:'home',
+  pathMatch:'full'
+},
+
+// Rota para 'home'
+{
+path:'home',
+component: HomeComponent,
+data:{ title: '' }
+},
+
+// Rota para 'news'
+{
+  path:'noticias',
+  component: NewsComponent,
+  data:{ title: 'Notícias' }
+},
+
+// Rota para 'contacts'
+{
+  path:'contatos',
+  component: ContactsComponent,
+  data:{ title: 'Faça contato' }
+},
+
+// Rota para 'about'
+{
+  path:'sobre',
+  component: AboutComponent,
+  data:{ title: 'Sobre' }
+},
+
+// Rota para página de erro.
+// Deve ser sempre a última rota
+{
+  path:'**',
+  component: E404Component,
+  data:{ title: 'Erro 404' }
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
