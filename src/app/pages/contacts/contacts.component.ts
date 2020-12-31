fdc5581fd8cd625ff4d8b0430d4f8d38f7795355
huldson2 <huldson2@gmail.com>
2020-12-30 22:52:35 -0300

    Ajustes 03
    Atualizado page de noticias e contatos.
    Incrementado a autenticação google

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Aula 11.2) Importa dedependências
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
@@ -7,9 +13,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  // Aula 11.2) Atributos do objeto
+  contactForm: FormGroup; // Formulário
+
+  // Aula 12) Cria um objeto de data
+  pipe = new DatePipe('en_US');
+
+  // Aula 12) Controla feedback ao usuário
+  feedback = false;
+
+  constructor(
+
+    // Aula 11.2) Injeta dependências
+    private form: FormBuilder,
+    private fbStore: AngularFirestore,
+    private fbAuth: AngularFireAuth,
+  ) { }
 
   ngOnInit(): void {
+
+    // Aula 11.2) Cria formulário de contatos
+    this.contactFormCreate();
+
+    // Aula 11.2) Preenche os campos do formulário se usuário estiver logado
+    if (this.contactForm) {
+      this.fbAuth.onAuthStateChanged((userData) => {
+        this.contactForm.controls.name.setValue(userData.displayName.trim());
+        this.contactForm.controls.email.setValue(userData.email.trim());
+      });
+    }
   }
 
+  // Aula 11.2) Cria formulário de contatos
+  contactFormCreate(): void {
+    this.contactForm = this.form.group({
+      date: [''],
+      name: [
+        '',
+
+        // Aula 11.2) Validando 'name'
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])
+      ],
+      email: [
+        '',
+
+        // Aula 11.2) Validando 'email'
+        Validators.compose([
+          Validators.required,
+          Validators.email
+        ])
+      ],
+      subject: [
+        '',
+
+        // Aula 11.2) Validando 'subject'
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4)
+        ])
+      ],
+      message: [
+        '',
+
+        // Aula 11.2) Validando 'subject'
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5)
+        ])
+      ],
+      status: ['enviado']
+    });
+  }
+
+  // Aula 11.2) Processa envio do formulário
+  // Aula 12) Processa envio do formulário
+  contactSend(): void {
+
+    // Criar data
+    this.contactForm.controls.date.setValue(
+      this.pipe.transform(Date.now(), 'yyyy-MM-dd hh:mm:ss').trim()
+    );
+
+    // Salvar no Firebase Cloud Firestore
+    this.fbStore.collection('contacts').add(this.contactForm.value)
+      .then(() => {
+
+        // Exibir feeback
+        this.feedback = true;
+
+        // Resetar formulário
+        this.contactForm.reset({
+          date: '',
+          name: this.contactForm.value.name.trim(),
+          email: this.contactForm.value.email.trim(),
+          subject: '',
+          message: '',
+          status: 'enviado'
+        });
+
+      })
+      .catch((error) => {
+        console.error('Erro ao salvar no Db:', error);
+      });
+  }
 }
--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Aula 11) Importa dependências
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  // Aula 11) Atributos
+  apiKey = '7c836b47084e46ed9468cecf44d420ba'; // APIKey obtida do 'newsapi.org'
+  apiQuery = 'cachorros'; // Palavra chave de busca
+  apiItens = 10; // Quantidade de notícias à exibir. No máximo de 20.
+
+  // Aula 11) Cuidado ao alterar
+  apiURL = `https://newsapi.org/v2/everything?apiKey=${this.apiKey}&source=google-news-br&language=pt&q=${this.apiQuery}`;
+  newsList: any;
+
+  constructor(
+
+    // Aula 11) Injeta dependências
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
-  }
 
+    // Aula 11) Obtém notícias da API REST
+    this.http.get(this.apiURL).subscribe(
+      (data: any) => {
+        this.newsList = data.articles.slice(0, this.apiItens);
+        // console.log(this.newsList);
+      }
+    );
+  }
 }
